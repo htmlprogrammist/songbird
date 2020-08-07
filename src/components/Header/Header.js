4163d3e0512d21b0f770ce470112a05d3f76a55c
Egor Badmaev <badmaeve2511@gmail.com>
2020-08-07 10:02:00 +0300

    feat: add header's example

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Разминка</li>
+          <li>Воробьиные</li>
+          <li>Лесные птицы</li>
+          <li>Певчие птицы</li>
+          <li>Хищные птицы</li>
+          <li>Морские птицы</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
